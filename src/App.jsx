54003636224a146ec2b9b2a773feabb6d8ecd551
doctype-Melvin <doctype.melvin@gmail.com>
2022-12-29 12:11:41 +0100

    dice are held on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,54 @@
-import React from "react"
+import React, { useState } from "react"
 import Die from "./components/Die"
 
-
 export default function App() {
+
+    const [dice, setDice] = useState(allNewDice())
+
+    
+    function allNewDice(min = 1, max = 6) {
+        let newDice = []
+        for (let i = 0; i < 10; i += 1) {
+            newDice.push({
+                value: Math.floor(Math.random() * max ) + min,
+                isHeld: false,
+                index: i
+            })
+        }
+        return newDice
+    }
+    
+    function rollDice() {
+        setDice(allNewDice())
+    }
+
+    
+    const holdDice = (id) => {
+        setDice(prevState => prevState.map(item => {
+            return item.index === id ? {...item, isHeld: !item.isHeld} : item
+        }))
+    }
+    
+    const diceRender = dice.map((item, index) => <Die
+    key={index}
+    id={index}
+    value={item.value} 
+    isHeld={item.isHeld}
+    holdDice={() => holdDice(index)}
+    />)
+
+
     return (
         <main className="mainContainer">
             <div className="diceContainer">
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
-                <Die value='6' />
+                {diceRender}
             </div>
+            <button 
+            className="rollBtn"
+            onClick={rollDice}
+            >
+                Roll Dice
+            </button>
         </main>
     )    
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 
 
-export default function Die({ value }) {
+export default function Die({ value, isHeld, holdDice }) {
     return (
-        <div className="dieContainer">
-            {value}
+        <div 
+        className={ isHeld ? "held" : "dieContainer"}
+        onClick={holdDice}>
+            <h2>
+            {value}    
+            </h2>
         </div>
     )
 }
