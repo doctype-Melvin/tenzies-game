ececdca9af44d5e882c448fb04ce7ba0b1626894
doctype-Melvin <doctype.melvin@gmail.com>
2022-12-29 09:18:18 +0100

    basic styling and manually added dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react"
+import Die from "./components/Die"
 
 
 export default function App() {
     return (
-        <div>
-            Hello World
-        </div>
+        <main className="mainContainer">
+            <div className="diceContainer">
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+                <Die value='6' />
+            </div>
+        </main>
     )    
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export default function Die({ value }) {
+    return (
+        <div className="dieContainer">
+            {value}
+        </div>
+    )
+}
